4475e5bae420c039f258aa8c1c47fdac78e60186
Jaques Candeias <jaques@cronic.pt>
2022-08-15 20:05:18 +0100

    add default value to taxable

--- src/app/features/channels/discogs/part-listing-summary/part-listing-summary.component.ts
+++ src/app/features/channels/discogs/part-listing-summary/part-listing-summary.component.ts
@@ -52,7 +52,7 @@ export class PartListingSummaryComponent implements OnInit {
     this.formGroup = new FormGroup({
       quantity: new FormControl({value: 1, disabled: this.disableQuantities}, Validators.required),
       price: new FormControl(null, Validators.required),
-      taxable: new FormControl(null, Validators.required),
+      taxable: new FormControl(false, Validators.required),
       barcode: new FormControl(this.release.barcode),
       sku: new FormControl(null),
       formatQuantity: new FormControl(this.release.formatQuantity),
